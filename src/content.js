5d703509a4a006c80da820ad1962fc1c12ae4998
Dan Ortega <ortega.dan2010@gmail.com>
2020-03-04 18:30:35 -0500

    Including the ability to query different dates in the week and code refactoring and variables renaming

--- src/content.js
+++ src/content.js
@@ -1,23 +1,31 @@
-var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-var now = new Date();
-console.log("" + now)
-
-var adder = 0
-
-var todaysTextDate = months[now.getMonth()] + " " + now.getDate() + ", " + now.getFullYear()
-console.log(todaysTextDate)
-
+var calendarTimeAdder = 0
 
 $("body").keypress(function (event) {
 
-    if (event.ctrlKey === true && event.key === "i") {
+    if (event.ctrlKey === true && (event.key === "i" | event.key === "I" | event.key === "k" | event.key === "K")) {
+
+        var requiredDayText
+
+        if (event.key === "i" | event.key === "I") {
+            var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            var todaysDate = new Date();
+            console.log("" + todaysDate)
+            requiredDayText = months[todaysDate.getMonth()] + " " + todaysDate.getDate() + ", " + todaysDate.getFullYear()
+        }
+
+        if (event.key === "k" | event.key === "K") {
+            requiredDayText = prompt("What Month and Day do you want to check ?\n[From the week on screen and in the format Month day#]")
+        }
+
+        console.log(requiredDayText)
+
 
         $("div.ynRLnc").each(function (index) {
 
 
             var texto = $(this).text()
 
-            if (texto.includes(todaysTextDate) && !texto.includes("Lunch")) {
+            if (texto.includes(requiredDayText) && !texto.includes("Lunch")) {
                 console.log(index + ") " + texto)
 
                 var startDateTime = new Date("01/01/2000 " + texto.substring(0, 5));
@@ -33,23 +41,21 @@ $("body").keypress(function (event) {
 
                 hoursLength = (endMinute - startMinute) / 60
 
-                adder += hoursLength
+                calendarTimeAdder += hoursLength
 
             }
         })
 
-        var message = "" + adder + " hours worked today"
+        alert("[" + requiredDayText + "]\n\n" + calendarTimeAdder + " hours worked")
 
-        alert(message)
-        if ((8 - adder) < 0) {
-            alert("You've worked " + (8 - adder) * -1 + " extra hours")
+        if ((8 - calendarTimeAdder) < 0) {
+            alert("[" + requiredDayText + "]\n\nYou've worked " + (8 - calendarTimeAdder) * -1 + " extra hours")
         } else {
-            alert("\nOnly " + (8 - adder) + " hours missing")
+            alert("[" + requiredDayText + "]\n\nOnly " + (8 - calendarTimeAdder) + " hours missing")
         }
-        adder = 0
+        calendarTimeAdder = 0
 
         console.log("Done ******\n\n")
     }
-
 
 })
