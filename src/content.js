37fd642ae5e8f317e27ef85be7178fb410f5840a
Dan Ortega <ortega.dan2010@gmail.com>
2020-05-22 11:47:28 -0500

    Putting counter into an external function

--- src/content.js
+++ src/content.js
@@ -6,64 +6,69 @@ var months = ['January', 'February', 'March', 'April', 'May', 'June',
     'July', 'August', 'September', 'October', 'November', 'December'];
 
 
-$("body").keypress(function (event) {
+function googleCounter(index) {
 
-    var calendarTimeAdder = 0
+    var texto = $(this).text()
+
+    if (texto.includes(requiredDayText) && !texto.includes("Lunch,") &&
+        !texto.includes("busy,") && !texto.includes("cal.ignore")) {
+
+        console.log(index + ") " + texto)
+
+        var startDateTime = new Date("01/01/2000 " + texto.substring(0, 5));
+        var endDateTime = new Date("01/01/2000 " + texto.substring(9, 14));
+
+
+        var startMinute = (startDateTime.getHours() * 60) + startDateTime.getMinutes()
+        var endMinute = (endDateTime.getHours() * 60) + endDateTime.getMinutes()
+
+
+        // console.log(startMinute)
+        // console.log(endMinute)
+
+        hoursLength = (endMinute - startMinute) / 60
+        console.log("Hours: " + hoursLength)
+        calendarTimeAdder += hoursLength
+
+    }
+}
+
+
+$("body").keypress(function (event) {
 
     if (event.ctrlKey === true && (event.key === "i" | event.key === "I"
         | event.key === "k" | event.key === "K")) {
 
         var requiredDayText
 
-        if (event.key === "i" | event.key === "I") {
+        // if (event.key === "i" | event.key === "I") {
 
-            var todaysDate = new Date();
-            console.log("" + todaysDate)
+        var todaysDate = new Date();
+        console.log("" + todaysDate)
 
-            if (enterpriseCalendar) {
-                requiredDayText = todaysDate.getDate() + " " + months[todaysDate.getMonth()] +
-                    " " + todaysDate.getFullYear()
-            }
-            else {
-                requiredDayText = months[todaysDate.getMonth()] + " " + todaysDate.getDate() +
-                    ", " + todaysDate.getFullYear()
-            }
+        if (enterpriseCalendar) {
+            requiredDayText = todaysDate.getDate() + " " + months[todaysDate.getMonth()] +
+                " " + todaysDate.getFullYear()
         }
+        else {
+            requiredDayText = months[todaysDate.getMonth()] + " " + todaysDate.getDate() +
+                ", " + todaysDate.getFullYear()
+        }
+        // }
 
         if (event.key === "k" | event.key === "K") {
-            requiredDayText = prompt("What Month and Day do you want to check ?" +
-                "\n[From the week on screen and in the format Month day#]")
+            requiredDayText = prompt("What " + (enterpriseCalendar ? "Day and Month" : "Month and Day") +
+                " do you want to check ?\n[From the week on screen and in the format: " +
+                (enterpriseCalendar ? "Month day#" : "day# Month") + "]")
         }
 
         console.log(requiredDayText)
 
 
-        $("div.ynRLnc").each(function (index) {
 
-            var texto = $(this).text()
+        var calendarTimeAdder = 0
+        $("div.ynRLnc").each(googleCounter)
 
-            if (texto.includes(requiredDayText) && !texto.includes("Lunch,") &&
-                !texto.includes("busy,") && !texto.includes("cal.ignore")) {
-
-                console.log(index + ") " + texto)
-
-                var startDateTime = new Date("01/01/2000 " + texto.substring(0, 5));
-                var endDateTime = new Date("01/01/2000 " + texto.substring(9, 14));
-
-
-                var startMinute = (startDateTime.getHours() * 60) + startDateTime.getMinutes()
-                var endMinute = (endDateTime.getHours() * 60) + endDateTime.getMinutes()
-
-
-                // console.log(startMinute)
-                // console.log(endMinute)
-
-                hoursLength = (endMinute - startMinute) / 60
-                console.log("Hours: " + hoursLength)
-                calendarTimeAdder += hoursLength
-
-            }
-        })
 
         if (requiredDayText == "" || calendarTimeAdder === 0) {
             alert("No times found for filter [" + requiredDayText + "]")
