5a66395db94805b8690e348b0ad56a462ced5532
Dan Ortega <ortega.dan2010@gmail.com>
2020-05-22 13:36:55 -0500

    Done with military time conversion and testing on nodejs

--- testing.js
+++ testing.js
@@ -1,13 +1,37 @@
 function convertToMilitaryTime(timeInAmPmFormatString) {
 
-    
+    var amOrPm = timeInAmPmFormatString.match("(am|pm)")[0];
+    var justTime = timeInAmPmFormatString.replace(amOrPm, "");
 
+    var cameWithMinutes = justTime.includes(":");
+    var hour = parseInt(cameWithMinutes ? justTime.split(":")[0] : justTime);
 
-    
+    var isPostMeridiem = amOrPm === "pm";
+    hour = isPostMeridiem ? hour + 12 : hour;
 
+    var finalTime = (cameWithMinutes ? ("" + hour) + ":" + justTime.split(":")[1] :
+        ("" + hour) + ":00");
 
-    return ""
+    finalTime = finalTime.padStart(5, "0");
+
+    return finalTime;
 }
 
 
-console.log(convertToMilitaryTime("8am"))
+var test;
+test = "8am";
+console.log("Test: " + test + ".  Result: " + convertToMilitaryTime(test));
+test = "8pm";
+console.log("Test: " + test + ".  Result: " + convertToMilitaryTime(test));
+test = "8:05am";
+console.log("Test: " + test + ".  Result: " + convertToMilitaryTime(test));
+test = "8:05pm";
+console.log("Test: " + test + ".  Result: " + convertToMilitaryTime(test));
+test = "11am";
+console.log("Test: " + test + ".  Result: " + convertToMilitaryTime(test));
+test = "11pm";
+console.log("Test: " + test + ".  Result: " + convertToMilitaryTime(test));
+test = "11:05am";
+console.log("Test: " + test + ".  Result: " + convertToMilitaryTime(test));
+test = "11:05pm";
+console.log("Test: " + test + ".  Result: " + convertToMilitaryTime(test));
