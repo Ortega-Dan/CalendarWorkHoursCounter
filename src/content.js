6fe2df76e81717782bc16eda477dbbff37192271
Dan Ortega <ortega.dan2010@gmail.com>
2020-05-23 13:36:38 -0500

    Filling it up with comments and displaying original query and original event text formats

--- src/content.js
+++ src/content.js
@@ -1,68 +1,86 @@
+// Finding if it is Google Calendar, or Enterprise (Google-Calendar based).
 var calendarType = document.getElementsByTagName("html")[0].getAttribute("data-base-title")
 // var calendarType = $("html").getAttribute("data-base-title")
-
 var enterpriseCalendar = !calendarType.includes("Google")
 
+// Setting a month conversion helper array
 var months = ['January', 'February', 'March', 'April', 'May', 'June',
     'July', 'August', 'September', 'October', 'November', 'December']
 
+/** Function used with enterprise calendar to convert from Meridiam time to Military time */
 function convertToMilitaryTime(timeInAmPmFormatString) {
 
+    // Extracting am or pm
     var amOrPm = timeInAmPmFormatString.match("(am|pm)")[0];
+    var isPostMeridiem = amOrPm === "pm";
+
+    // Extracting the time
     var justTime = timeInAmPmFormatString.replace(amOrPm, "");
 
+    // Finding if it came with minuts and the hour
     var cameWithMinutes = justTime.includes(":");
     var hour = parseInt(cameWithMinutes ? justTime.split(":")[0] : justTime);
 
-    var isPostMeridiem = amOrPm === "pm";
-
+    // Converting the hour to a 24 hour standard
     hour = isPostMeridiem && hour !== 12 ? hour + 12 : hour;
     hour = hour === 12 && !isPostMeridiem ? 0 : hour;
 
+    // Adding the minutes to the hour
     var finalTime = (cameWithMinutes ? ("" + hour) + ":" + justTime.split(":")[1] :
         ("" + hour) + ":00");
 
+    // Padding to 2 digits in the hour
     finalTime = finalTime.padStart(5, "0");
 
     return finalTime;
 }
 
+// Listening to keypress events in the entire document
+$("html").keypress(function (event) {
 
-$("body").keypress(function (event) {
-
+    // Running functionality con Ctrl + i or Ctrl + k (case insensitive)
     if (event.ctrlKey === true && (event.key === "i" | event.key === "I"
         | event.key === "k" | event.key === "K")) {
 
+        // Variable to hold query (entered or inferred)
         var requiredDayText
 
+        // Set the query for the current day
         if (event.key === "i" | event.key === "I") {
 
             var todaysDate = new Date()
 
+            // Enterprise Calendar format
             if (enterpriseCalendar) {
                 requiredDayText = todaysDate.getDate() + " " + months[todaysDate.getMonth()] +
                     " " + todaysDate.getFullYear()
             }
+            // Google Calendar format
             else {
                 requiredDayText = months[todaysDate.getMonth()] + " " + todaysDate.getDate() +
                     ", " + todaysDate.getFullYear()
             }
         }
 
+        // Prompting for the query when user requested
         if (event.key === "k" | event.key === "K") {
+            // Enterprise query should be Day and Month, whereas Google Calendar should be Month and Day
             requiredDayText = prompt("What " + (enterpriseCalendar ? "Day and Month" : "Month and Day") +
                 " do you want to check ?\n[From the week on screen and in the format: " +
                 (enterpriseCalendar ? "day# Month" : "Month day#") + "]\n\n" +
                 "Or leave empty to check the entire week.")
         }
 
-        requiredDayText = requiredDayText.toLowerCase()
-
+        // Logging the query (entered or inferred)
         console.log("Checking " + (requiredDayText == "" ? "the week displayed" : requiredDayText) + ".")
 
+        // Converting the query to lower case for later case insensitive matching
+        requiredDayText = requiredDayText.toLowerCase()
 
+        // Initializing variable to add times
         var calendarTimeAdder = 0
 
+        // Function implementation for Enterprise calendar
         if (!enterpriseCalendar)
             $("div.ynRLnc").each(function (index) {
 
@@ -70,12 +88,13 @@ $("body").keypress(function (event) {
 
                 if (texto === "") { return }
 
+                originalText = texto
                 texto = texto.toLowerCase()
 
                 if (texto.includes(requiredDayText) && !texto.includes("Lunch,") &&
                     !texto.includes("busy,") && !texto.includes("cal.ignore")) {
 
-                    console.log(++index + ") " + texto)
+                    console.log(++index + ") " + originalText)
 
                     var startDateTime = new Date("01/01/2000 " + texto.substring(0, 5));
                     var endDateTime = new Date("01/01/2000 " + texto.substring(9, 14));
@@ -95,18 +114,20 @@ $("body").keypress(function (event) {
                 }
             })
 
+        // Function implementation for Google Calendar
         else
             $("div.ynRLnc").each(function (index) {
                 var texto = $(this).text()
 
                 if (texto === "") { return }
 
+                originalText = texto
                 texto = texto.toLowerCase()
 
                 if (texto.includes(requiredDayText) && !texto.includes("Lunch,") &&
                     !texto.includes("busy,") && !texto.includes("cal.ignore")) {
 
-                    console.log(++index + ") " + texto)
+                    console.log(++index + ") " + originalText)
 
                     var fromTime = texto.split(" ")[0]
                     var toTime = texto.split(" ")[2]
@@ -135,11 +156,13 @@ $("body").keypress(function (event) {
 
         console.log("Total time: " + calendarTimeAdder + " hours.");
 
+        // Showing results
         if (calendarTimeAdder === 0) {
             alert("No times found for filter [" + requiredDayText + "]")
         } else {
             alert("[" + requiredDayText + "]\n\n" + calendarTimeAdder + " hours worked")
 
+            // Only show missing hours for single day queries (not for entire week)
             if (requiredDayText != "") {
 
                 if ((8 - calendarTimeAdder) < 0) {
