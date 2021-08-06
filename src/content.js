cc73f91d631041688884bce0c18436b0e809fd68
Dan Ortega <ortega.dan2010@gmail.com>
2021-08-06 10:05:19 -0500

    Adjusting interface

--- src/content.js
+++ src/content.js
@@ -169,28 +169,22 @@ $("html").keydown(function (event) {
 
     }
 
+    // insert first prefix
     if (event.altKey === true && event.code === "KeyJ") {
-
         chrome.storage.sync.get(["first"], function (result) {
-
             // console.log('first value is ' + result["first"]);
             document.activeElement.nextSibling.textContent = ""
             document.activeElement.value = result["first"]
-
         });
-
     }
 
+    // insert second prefix
     if (event.altKey === true && event.code === "KeyL") {
-
         chrome.storage.sync.get(["second"], function (result) {
-
             // console.log('second value is ' + result["second"]);
             document.activeElement.nextSibling.textContent = ""
             document.activeElement.value = result["second"]
-
         });
-
     }
 
 })
